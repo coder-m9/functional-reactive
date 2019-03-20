7714b183e3ac1e4952cb61b752e2cbd80037e653
coder-m9 <35654636+coder-m9@users.noreply.github.com>
2019-03-20 18:55:54 +0530

    Stream

---
+++ StreamTest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class StreamTest {
+
+public static void main( String arg[])
+{
+List<String> myList =
+    Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+myList
+    .stream()
+    .filter(s -> s.startsWith("c"))
+    .map(String::toUpperCase)
+    .sorted()
+    .forEach(System.out::println);
+}
+
+}
